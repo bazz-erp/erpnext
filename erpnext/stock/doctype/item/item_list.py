9bf01b5a9678865fffdc61b851ecafbea9deaf1e
Juan Cruz Gardey <juan.gardey@ubykuo.com>
2018-03-08 17:51:13 -0300

    standard rate to be showed in item list is calculated using default_pricelist field of Stock Settings

--- erpnext/stock/doctype/item/item_list.py
+++ erpnext/stock/doctype/item/item_list.py
@@ -1,8 +1,10 @@
+from __future__ import unicode_literals
 import frappe
 from frappe.desk.reportview import *
 
 @frappe.whitelist()
 def get_items():
     args = get_form_params()
-    args["fields"].append("""(select price_list_rate from `tabItem Price` ip where ip.parent=`tabItem`.`name` and ip.price_list='Compra Estandar')  as standard_rate""")
+    price_list_showed = frappe.db.get_single_value("Stock Settings", "default_pricelist")
+    args["fields"].append("""(select price_list_rate from `tabItem Price` ip where ip.parent=`tabItem`.`name` and ip.price_list='{}')  as standard_rate""".format(price_list_showed))
     return compress(execute(**args), args = args)
