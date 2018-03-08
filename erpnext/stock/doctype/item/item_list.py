68e11d554ff6d30495a5ee58b12fb33c7ee7df0d
Juan Cruz Gardey <juan.gardey@ubykuo.com>
2018-03-08 18:07:37 -0300

    standard rate is set to 0 when not exists a rate for a given price list

--- erpnext/stock/doctype/item/item_list.py
+++ erpnext/stock/doctype/item/item_list.py
@@ -6,5 +6,5 @@ from frappe.desk.reportview import *
 def get_items():
     args = get_form_params()
     price_list_showed = frappe.db.get_single_value("Stock Settings", "default_pricelist")
-    args["fields"].append("""(select price_list_rate from `tabItem Price` ip where ip.parent=`tabItem`.`name` and ip.price_list='{}')  as standard_rate""".format(price_list_showed))
+    args["fields"].append("""(ifnull((select price_list_rate from `tabItem Price` ip where ip.parent=`tabItem`.`name` and ip.price_list='{}'),0))  as standard_rate""".format(price_list_showed))
     return compress(execute(**args), args = args)
