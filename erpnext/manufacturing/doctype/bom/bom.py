d360c1be91764bdb0552374056dad213c5e70ab4
Juan Cruz Gardey <juan.gardey@ubykuo.com>
2018-03-18 12:39:08 -0300

    added amount column when calculating BOM items for a specified qty

--- erpnext/manufacturing/doctype/bom/bom.py
+++ erpnext/manufacturing/doctype/bom/bom.py
@@ -513,8 +513,7 @@ def get_bom_items_as_dict(bom, company, qty=1, fetch_exploded=1, fetch_scrap_ite
                 item.default_production_warehouse,
                 item.expense_account as expense_account,
                 item.buying_cost_center as cost_center,
-                bom_item.rate,
-                bom_item.amount
+                (bom_item.amount * %(qty)s)/ifnull(bom.quantity, 1) as amount
                 {select_columns}
             from
                 `tab{table}` bom_item, `tabBOM` bom, `tabItem` item
