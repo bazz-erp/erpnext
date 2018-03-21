6e8ad943655eb8004079c3f9bd7064f7a34d755d
Juan Cruz Gardey <juan.gardey@ubykuo.com>
2018-03-21 18:56:44 -0300

    - added payment entries to Workshop Current Account. - code refactored

--- erpnext/manufacturing/report/workshop_current_account/workshop_current_account.py
+++ erpnext/manufacturing/report/workshop_current_account/workshop_current_account.py
@@ -27,18 +27,18 @@ def get_data(filters):
     if not (filters.get("workshop") and filters.get("company")):
         frappe.throw(_("{0} and {1} are mandatory".format(_("Workshop"), _("Company"))))
 
-    entries = frappe.db.sql("""SELECT item.item_name, sd.item_code, sd.qty, sd.amount, sd.s_warehouse, 
-    sd.t_warehouse, sd.uom, se.posting_date, se.purpose FROM `tabStock Entry` as se, 
-`tabStock Entry Detail` as sd, `tabItem` as item, `tabProduction Order` as po, 
-`tabOperation Completion` as op where op.workshop = %(workshop)s and sd.parent = se.name
-and se.operation = op.name and sd.item_code = item.name and op.production_order=po.name 
-and po.company=%(company)s and (se.purpose = 'Manufacturer Shipping' or se.purpose = 'Manufacturer Receipt') 
-order by se.posting_date, se.creation""", filters, as_dict=1)
+    payment_entries = get_workshop_payment_entries(filters)
 
+    stock_entry_details = get_stock_entry_details(filters)
+
+    entries = payment_entries + stock_entry_details
     balance = 0
-    for entry in entries:
-        entry["debit"] = entry.get("amount") if entry.get("purpose") == "Manufacturer Shipping" else 0
+    for entry in sorted(entries, key=lambda e: e["creation"]):
+        entry["debit"] = entry.get("amount") if (entry.get("purpose") == "Manufacturer Shipping"
+                                                 or entry.get("purpose") == "Payment") else 0
+
         entry["credit"] = entry.get("amount") if entry.get("purpose") == "Manufacturer Receipt" else 0
+
         balance += entry["debit"] - entry["credit"]
         data.append([_(entry.get("purpose")), entry.get("posting_date"), entry.get("item_code"),
                      entry.get("item_name"), entry.get("qty"), entry.get("uom"), entry.get("debit"), entry.get("credit"), balance])
@@ -46,3 +46,21 @@ order by se.posting_date, se.creation""", filters, as_dict=1)
     data.append([])
     data.append([_("Total"),None, None,None,None,None,None, None, balance])
     return data
+
+def get_stock_entry_details(filters):
+    return frappe.db.sql("""SELECT sd.creation, item.item_name, sd.item_code, sd.qty, sd.amount, sd.s_warehouse, 
+            sd.t_warehouse, sd.uom, se.posting_date, se.purpose FROM `tabStock Entry` as se, 
+        `tabStock Entry Detail` as sd, `tabItem` as item, `tabProduction Order` as po, 
+        `tabOperation Completion` as op
+        where op.workshop = %(workshop)s and sd.parent = se.name
+        and se.operation = op.name and sd.item_code = item.name and op.production_order=po.name 
+        and po.company=%(company)s and (se.purpose = 'Manufacturer Shipping' or se.purpose = 'Manufacturer Receipt') 
+        order by se.posting_date, sd.creation""", filters, as_dict=1)
+
+
+
+
+def get_workshop_payment_entries(filters):
+    return frappe.db.sql("""select creation, posting_date, paid_amount as amount, 'Payment' as purpose from `tabPayment Entry` 
+where party_type='Supplier' and party=%(workshop)s order by posting_date, creation""", filters, as_dict=1)
+
