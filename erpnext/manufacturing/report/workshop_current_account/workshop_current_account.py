0a143d499353fbb98f5c4297d6a24aa498ac813c
Juan Cruz Gardey <juan.gardey@ubykuo.com>
2018-03-21 17:52:44 -0300

    workshop current account: - added UOM column. - modified query to show stock entry details of all products (production item and raw materials)

--- erpnext/manufacturing/report/workshop_current_account/workshop_current_account.py
+++ erpnext/manufacturing/report/workshop_current_account/workshop_current_account.py
@@ -16,6 +16,7 @@ def get_columns():
         _("Item Code") + ":Link/Item:80",
         _("Item Name") + ":Data:140",
         _("Qty") + ":Float:80",
+        _("UOM") + ":Link/UOM:80",
         _("Debit") + ":Currency:80",
         _("Credit") + ":Currency:80",
         _("Balance") + ":Currency:80"
@@ -27,12 +28,12 @@ def get_data(filters):
         frappe.throw(_("{0} and {1} are mandatory".format(_("Workshop"), _("Company"))))
 
     entries = frappe.db.sql("""SELECT item.item_name, sd.item_code, sd.qty, sd.amount, sd.s_warehouse, 
-    sd.t_warehouse, se.posting_date, se.purpose FROM `tabStock Entry` as se, 
+    sd.t_warehouse, sd.uom, se.posting_date, se.purpose FROM `tabStock Entry` as se, 
 `tabStock Entry Detail` as sd, `tabItem` as item, `tabProduction Order` as po, 
 `tabOperation Completion` as op where op.workshop = %(workshop)s and sd.parent = se.name
 and se.operation = op.name and sd.item_code = item.name and op.production_order=po.name 
-and po.company=%(company)s and t_warehouse is not null 
-order by se.posting_date""", filters, as_dict=1)
+and po.company=%(company)s and (se.purpose = 'Manufacturer Shipping' or se.purpose = 'Manufacturer Receipt') 
+order by se.posting_date, se.creation""", filters, as_dict=1)
 
     balance = 0
     for entry in entries:
@@ -40,8 +41,8 @@ order by se.posting_date""", filters, as_dict=1)
         entry["credit"] = entry.get("amount") if entry.get("purpose") == "Manufacturer Receipt" else 0
         balance += entry["debit"] - entry["credit"]
         data.append([_(entry.get("purpose")), entry.get("posting_date"), entry.get("item_code"),
-                     entry.get("item_name"), entry.get("qty"), entry.get("debit"), entry.get("credit"), balance])
+                     entry.get("item_name"), entry.get("qty"), entry.get("uom"), entry.get("debit"), entry.get("credit"), balance])
 
     data.append([])
-    data.append([_("Total"),None, None,None,None,None, None, balance])
+    data.append([_("Total"),None, None,None,None,None,None, None, balance])
     return data
